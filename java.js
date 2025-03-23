7c9097f7c93357dd776a4bad1b995e9e4df97221
Shahryar-17 <shahryarjh.k@gmail.com>
2025-03-24 00:03:31 +0500

    .

---
+++ java.js
@@ -0,0 +1,35 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            const offsetPosition = targetElement.getBoundingClientRect().top + window.scrollY - 130;
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth',
+            });
+        }
+    });
+});
+
+// let atoz = document.getElementsByClassName('sticky')[0];
+// let atoz_content = document.getElementsByClassName('atoz')[0];
+
+// window.onscroll = () => {
+//     let scrollTop = window.scrollY;
+//     let viewportHeight = window.innerHeight;
+//     let contentHeight = atoz_content.getBoundingClientRect().height;
+
+//     if (scrollTop >= contentHeight - viewportHeight) {
+//         atoz_content.style.transform = `translateY(-${contentHeight - viewportHeight}px)`;
+//         atoz_content.style.position = 'fixed';
+//         atoz_content.className = 'edit';
+//     }
+//     else {
+//         atoz_content.style.transform = '';
+//         atoz_content.style.position = '';
+//     }
+// }
