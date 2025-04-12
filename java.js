64506e82f116f19403c1de281c40759ea2d1e230
Shahryar-17 <shahryarjh.k@gmail.com>
2025-04-13 01:00:58 +0500

    .

--- java.js
+++ java.js
@@ -33,3 +33,35 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 //         atoz_content.style.position = '';
 //     }
 // }
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+
+    // If we need pagination
+    pagination: {
+        // el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 4
+        },
+        768: {
+            slidesPerView: 10
+        },
+        1024: {
+            slidesPerView: 10
+        }
+    }
+});
