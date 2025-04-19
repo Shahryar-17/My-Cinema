f90348fef46584283cdd1fae7c21d87e332f91c4
Shahryar-17 <shahryarjh.k@gmail.com>
2025-04-19 19:51:57 +0500

    .

---
+++ movies-java.js
@@ -0,0 +1,66 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            const offsetPosition = targetElement.getBoundingClientRect().top + window.scrollY - 130;
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth',
+            });
+        }
+    });
+});
+
+// let atoz = document.getElementsByClassName('sticky')[0];
+// let atoz_content = document.getElementsByClassName('atoz')[0];
+
+// window.onscroll = () => {
+//     let scrollTop = window.scrollY;
+//     let viewportHeight = window.innerHeight;
+//     let contentHeight = atoz_content.getBoundingClientRect().height;
+
+//     if (scrollTop >= contentHeight - viewportHeight) {
+//         atoz_content.style.transform = `translateY(-${contentHeight - viewportHeight}px)`;
+//         atoz_content.style.position = 'fixed';
+//         atoz_content.className = 'edit';
+//     }
+//     else {
+//         atoz_content.style.transform = '';
+//         atoz_content.style.position = '';
+//     }
+// }
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+
+    // If we need pagination
+    pagination: {
+        // el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 3
+        },
+        768: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 7
+        }
+    }
+});
