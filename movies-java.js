47210512017ab41c9bf280cfd093a80e9d942381
Shahryar-17 <shahryarjh.k@gmail.com>
2025-04-20 01:21:22 +0500

    .

--- movies-java.js
+++ movies-java.js
@@ -64,3 +64,48 @@ const swiper = new Swiper('.slider-wrapper', {
         }
     }
 });
+
+const tabsBox = document.querySelector(".tabs-box");
+allTabs = document.querySelectorAll(".tab");
+arrowIcons = document.querySelectorAll(".icon i");
+
+let isDragging = false;
+
+const handleIcons = () => {
+    let scrollVal = Math.round(tabsBox.scrollLeft);
+    let maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
+    arrowIcons[0].parentElement.style.display = scrollVal > 0 ? "flex" : "none";
+    arrowIcons[1].parentElement.style.display = maxScrollableWidth > scrollVal ? "flex" : "none";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        // If clicked icon is left, reduce 350 from tabsBox scrollLeft else add
+        tabsBox.scrollLeft += icon.id === "left" ? -200 : 200;
+        setTimeout(() => handleIcons(), 50); // Calling handleIcons after 50 miliseconds
+    })
+})
+
+allTabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        // Removing active class from the previous tab & adding to current clicked tab
+        tabsBox.querySelector(".active").classList.remove("active");
+        tab.classList.add("active");
+    })
+})
+
+const dragging = (e) => {
+    if (!isDragging) return;
+    tabsBox.classList.add("dragging");
+    tabsBox.scrollLeft -= e.movementX;
+    handleIcons();
+}
+
+const dragStop = () => {
+    isDragging = false;
+    tabsBox.classList.remove("dragging");
+}
+
+tabsBox.addEventListener("mousedown", () => isDragging = true);
+tabsBox.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
